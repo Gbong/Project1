77f567a96a70bf0cd59a1f7e38010547c4f9f81e
Gbong <wjdtkddnjss@naver.com>
2016-06-22 18:08:43 +0900

    change

---
+++ Script/Warp002.js
@@ -0,0 +1,12 @@
+﻿var warptarget2 : Transform;
+
+function Update () {
+
+}
+
+function OnTriggerEnter (col : Collider) {
+
+    if (col.gameObject.tag == "warp002") {
+        this.transform.position = warptarget2.position;
+    }
+}
